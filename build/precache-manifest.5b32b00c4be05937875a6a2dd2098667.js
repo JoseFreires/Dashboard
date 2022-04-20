35295336d7758b0130576119efdd4ba036049507
Jose_Freires <88195769+JoseFreires@users.noreply.github.com>
2022-04-19 21:42:43 -0300

    First Commit

---
+++ build/precache-manifest.5b32b00c4be05937875a6a2dd2098667.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0903a7ca776f6cedb286",
+    "url": "/static/css/main.7dafd9e2.chunk.css"
+  },
+  {
+    "revision": "0903a7ca776f6cedb286",
+    "url": "/static/js/main.0903a7ca.chunk.js"
+  },
+  {
+    "revision": "4e4a363b3e81c963c5fa",
+    "url": "/static/js/1.4e4a363b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0409bad1ceb90f6bf9e11d1526b88fc2",
+    "url": "/static/media/login.0409bad1.svg"
+  },
+  {
+    "revision": "2e8f129490fbdb79da5f827c418ca7c8",
+    "url": "/static/media/logo_P.2e8f1294.png"
+  },
+  {
+    "revision": "95da147faabdec5d51864bb1aff9da45",
+    "url": "/index.html"
+  }
+];
